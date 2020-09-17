0ea90deb9653d6a945f4c5ffc8b9ccad9bc3c2f9
Larry X <papadocarock@gmail.com>
2020-09-17 16:30:54 -0400

    creategeolocation

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ $(function() {
     $("#search-btn").on("click", function(event) {
         event.preventDefault();
         showRecipe();
+        showRestaurant();
     })
 
     function showRecipe() {
@@ -26,4 +27,39 @@ $(function() {
     }
 
 
-})
+    function success(pos) {
+        var latitudeCrd = pos.coords.latitude;
+        var longitudeCrd = pos.coords.longitude;
+        localStorage.setItem("latitude", latitudeCrd);
+        localStorage.setItem("longitude", longitudeCrd);
+    }
+    navigator.geolocation.getCurrentPosition(success);
+    var service;
+    var infowindow;
+
+    function initialize() {
+        var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: pyrmont,
+            zoom: 15
+        });
+        var request = {
+            location: pyrmont,
+            radius: '500',
+            type: ['restaurant']
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+    }
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+        }
+    }
+    initialize();
+
+
+});
