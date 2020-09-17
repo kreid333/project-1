9992ec6a7f1e3bf441ff54c840e99ac1169b7a82
Patrick Hannan <patrick@hannan.com>
2020-09-17 12:39:16 -0400

    Added function to show recipe.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,17 @@ $(function() {
         
     })
 
+    function showRecipe() {
+        var queryURLRecipe = "";
+// api key = d966ad9d26074fb7a91baf2b851421b0
+        $.ajax({
+            url: queryURLRecipe,
+            method: "GET",
+        }).then(function (response) {
+    
+        })
+    }
+
 
 
 
